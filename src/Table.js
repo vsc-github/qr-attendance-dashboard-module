76b4802d8dac8d39a10f1d0e33043aeb47cb3941
vishwas <batty.wayn3@gmail.com>
2019-04-15 08:40:23 +0530

    Add datatable initializer

--- src/Table.js
+++ src/Table.js
@@ -1,4 +1,5 @@
 /*global $*/
+/*global jQuery*/
 import React, { Component } from 'react';
 import axios from 'axios';
 
@@ -8,8 +9,12 @@ class Table extends Component {
     constructor(){
         super();
         this.state = {
-            list: null
-        }
+            list: null,
+            attendanceType: '00',
+        }        
+        
+        this.handleInput = this.handleInput.bind(this);
+        this.getAttendance = this.getAttendance.bind(this);
     }
 
     componentDidMount(){
@@ -22,18 +27,72 @@ class Table extends Component {
                         list: res1.data,
                     })
             })
+
+            this.initializer = setInterval(()=>{
+                this.initializeDataTable();
+            },1000)
+    }
+
+    initializeDataTable(){
+        console.log("Called");
+        if( this.state.list !== null && typeof jQuery !== 'undefined' && jQuery().dataTable !== undefined ){
+            $('#example').DataTable();
+            clearInterval(this.initializer);
+        }
+    }
+
+    handleInput(e) {
+        this.setState({
+            [e.target.id]: e.target.value,
+        })
+    }
+
+    getAttendance(timestamps){
+        if (timestamps === "" || timestamps === undefined) return 0;
+
+        if( this.state.attendanceType === '00' ) {
+            return Object.keys(timestamps).length;
+        }
+        else {
+            let attendance = 0;
+            Object.keys(timestamps).map((key, index) => {
+                if(key.split('-')[1] === this.state.attendanceType){
+                    ++attendance;
+                }
+              });
+              return attendance;
+        }
+
     }
 
     render() {
         return (<div>
-            <button onClick={() => {
-                $('#example').DataTable();
-            }}>
-                Convert Table
-          </button>
-
             {
-                this.state.list === null ? null : (<div className="container" style={{ maxWidth: '70rem' }}>
+                this.state.list === null ? null : (<div className="container" style={{ maxWidth: '70rem', margin: '4rem auto 10rem auto' }}>
+                
+                <div className="field">
+                    <label className="label">Select Attendance Type:</label>
+                    <div className="control">
+                        <div className="select">
+                            <select id="attendanceType" onChange={this.handleInput}  value={this.state.attendanceType}>
+                                        <option value="00">Total</option>
+                                        <option value="01">January</option>
+                                        <option value="02">Februrary</option>
+                                        <option value="03">March</option>
+                                        <option value="04">April</option>
+                                        <option value="05">May</option>
+                                        <option value="06">June</option>
+                                        <option value="07">July</option>
+                                        <option value="08">August</option>
+                                        <option value="09">Sepetember</option>
+                                        <option value="10">October</option>
+                                        <option value="11">November</option>
+                                        <option value="12">December</option>
+                            </select> 
+                        </div>
+                    </div>
+                </div>
+                
                 <table id="example" className="display">
                         <thead>
                             <tr>
@@ -43,20 +102,20 @@ class Table extends Component {
                                 <th>Branch</th>
                                 <th>Year</th>
                                 <th>Section</th>
-                                <th>Net Attendance</th>
+                                <th>Attendance</th>
                             </tr>
                         </thead>
                         <tbody>
                             {
                                 this.state.list === null ? null : Object.keys(this.state.list).map((key, index) => {
-                                    return (<tr> 
+                                    return (<tr key={index}> 
                                         <td>{ this.state.list[key].roll }</td>
                                         <td>{ this.state.list[key].name }</td>
                                         <td>{ this.state.list[key].dob }</td>
                                         <td>{ this.state.list[key].branch }</td>
                                         <td>{ this.state.list[key].year }</td>
                                         <td>{ this.state.list[key].section }</td>
-                                        <td>{ (this.state.list[key].timestamps === "" || this.state.list[key].timestamps === undefined) ? 0 : Object.keys(this.state.list[key].timestamps).length }</td>
+                                        <td>{ this.getAttendance(this.state.list[key].timestamps) }</td>
                                         </tr>)
                                   })
                             }
